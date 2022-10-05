9a6aac79e21a75a651ccc0735902f7fdc031505e
zhouxianyuan <zxy965211@163.com>
2022-10-06 01:10:01 +0800

    ZABU_exp

--- src/test/ZABU_exp.sol
+++ src/test/ZABU_exp.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.10;
 import "forge-std/Test.sol";
 import "./interface.sol";
 
+// @Analysis
+// https://slowmist.medium.com/brief-analysis-of-zabu-finance-being-hacked-44243919ea29
+
 interface ZABUFarm{
     function deposit(uint256 _pid, uint256 _amount) external;
     function withdraw(uint256 _pid, uint256 _amount) external;
