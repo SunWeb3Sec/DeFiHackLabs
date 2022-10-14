104953de7fb2437af02c3504adc803fde32fa401
zhouxianyuan <zxy965211@163.com>
2022-10-14 23:45:52 +0800

    fix rpc

--- src/test/RADT_exp.sol
+++ src/test/RADT_exp.sol
@@ -28,7 +28,7 @@ contract ContractTest is DSTest{
     CheatCodes cheats = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
     function setUp() public {
-        cheats.createSelectFork("https://rpc.ankr.com/bsc", 21572418);
+        cheats.createSelectFork("bsc", 21572418);
     }
 
    
