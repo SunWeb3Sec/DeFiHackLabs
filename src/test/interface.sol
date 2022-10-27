fbda4d1b01f05a847d2f6462a4ff3fab7bfc3de2
zhouxianyuan <zxy965211@163.com>
2022-10-27 19:14:15 +0800

    N00d_exp

--- src/test/interface.sol
+++ src/test/interface.sol
@@ -746,6 +746,10 @@ interface IERC777 {
 
   function balanceOf(address owner) external view returns (uint256);
 
+  function approve(address spender, uint256 value) external returns (bool);
+
+  function transfer(address to, uint256 value) external returns (bool);
+
   function send(
     address recipient,
     uint256 amount,
