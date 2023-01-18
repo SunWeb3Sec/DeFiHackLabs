94efafb9eaae946f1d8472a529ed86df7ffe4a1f
eugenioclrc <eugenioclrc@gmail.com>
2023-01-18 16:16:10 -0300

    add skim to univ2pair

--- src/test/interface.sol
+++ src/test/interface.sol
@@ -442,11 +442,12 @@ interface IUniswapV2Factory {
 }
 interface IUniswapV2Pair {
     function swap(
-    uint256 amount0Out,
-    uint256 amount1Out,
-    address to,
-    bytes calldata data
-  ) external;
+      uint256 amount0Out,
+      uint256 amount1Out,
+      address to,
+      bytes calldata data
+    ) external;
+    function skim(address to) external;
     function token0() external view returns (address);
     function token1() external view returns (address);
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
