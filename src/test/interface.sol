a80f04f6b0a6183736943216746d8dea698188ac
joker <ox0x9k@gmail.com>
2023-02-28 00:23:01 +0800

    add cftoken_exp

--- src/test/interface.sol
+++ src/test/interface.sol
@@ -5234,3 +5234,16 @@ interface IOpyn{
 
     function removeUnderlying() external;
 }
+
+interface ICFToken {
+    function _transfer(
+        address from,
+        address to,
+        uint256 amount
+    ) external;
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount)
+        external
+        returns (bool);
+
+}
