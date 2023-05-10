5e7ae805c128b339b7b323df3ff9996f201e6783
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2023-05-10 10:17:57 +0800

    Update Poc-template.sol

--- src/Poc-template.sol
+++ src/Poc-template.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
+/*
 
 import "forge-std/Test.sol";
 import "./interface.sol";
@@ -35,3 +36,5 @@ contract ContractTest is Test {
         //cheats.stopPrank();
     }
 }
+
+*/
