00ca647dc52aa95e21ef37922086512c0545ac83
SunWeb3Sec <107249780+SunWeb3Sec@users.noreply.github.com>
2023-05-10 10:09:19 +0800

    Update and rename Contract.sol to Poc-template.sol

---
+++ src/Poc-template.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+import "forge-std/Test.sol";
+import "./interface.sol";
+
+// @KeyInfo - Total Lost : ~999M US$
+// Attacker : 0xcafebabe
+// Attack Contract : 0xdeadbeef
+// Vulnerable Contract : 0xdeadbeef
+// Attack Tx : 0x123456789
+
+// @Info
+// Vulnerable Contract Code : https://etherscan.io/address/0xdeadbeef#code
+
+// @Analysis
+// Post-mortem : https://www.google.com/
+// Twitter Guy : https://www.google.com/
+// Hacking God : https://www.google.com/
+
+contract ContractTest is Test {
+    IERC20 USDC = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+    IUSDT USDT = IUSDT(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+    CheatCodes cheats = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    
+    function setUp() public {
+        cheats.createSelectFork("mainnet", 15460093);
+        cheats.label(address(USDT), "USDT");
+        cheats.label(address(USDC), "USDC");
+        //address alice = makeAddr("alice");
+    }
+    
+    function testExploit() public{
+        //cheats.startPrank(alice);
+        //cheats.stopPrank();
+    }
+}
