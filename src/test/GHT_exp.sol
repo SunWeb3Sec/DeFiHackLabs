8b12c14a6a3b9a4a6ea3805fe76d28fd5aa84579
bixia <343224563@qq.com>
2024-03-07 22:51:48 +0800

    update

--- src/test/GHT_exp.sol
+++ src/test/GHT_exp.sol
@@ -10,16 +10,25 @@ import "./interface.sol";
 // Vuln Contract : https://etherscan.io/address/0x528e046acfb52bd3f9c400e7a5c79a8a2c2863d0
 // Attack Tx : https://phalcon.blocksec.com/explorer/tx/eth/0xd17266bcdf30cbcbd7d0b5a006f43141981aeee2e1f860f68c9a1805ecacbc68?line=3
 interface IGHT {
-    function transferFrom(address, address, uint) external;
-    function balanceOf(address) external view returns (uint);
+    function transferFrom(address, address, uint256) external;
+    function balanceOf(address) external view returns (uint256);
 }
+
+interface CheatCodesNew {
+    /// Creates and also selects new fork with the given endpoint and at the block the given transaction was mined in,
+    /// replays all transaction mined in the block before the transaction, returns the identifier of the fork.
+    function createSelectFork(string calldata urlOrAlias, bytes32 txHash) external returns (uint256 forkId);
+}
+
 contract ContractTest is Test {
     WETH9 private constant WETH = WETH9(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
     IGHT private constant GHT = IGHT(0x528e046ACfb52bD3f9c400e7A5c79A8a2c2863d0);
     Uni_Pair_V2 private constant WETH_GHT = Uni_Pair_V2(0x706206EabD6A70ca4992eEc1646B6D1599259CAe);
 
     function setUp() public {
-        vm.createSelectFork("mainnet", bytes32(0xd17266bcdf30cbcbd7d0b5a006f43141981aeee2e1f860f68c9a1805ecacbc68));
+        CheatCodesNew(address(vm)).createSelectFork(
+            "mainnet", bytes32(0xd17266bcdf30cbcbd7d0b5a006f43141981aeee2e1f860f68c9a1805ecacbc68)
+        );
         vm.label(address(WETH), "WETH");
         vm.label(address(GHT), "GHT");
         vm.label(address(WETH_GHT), "WETH_GHT");
