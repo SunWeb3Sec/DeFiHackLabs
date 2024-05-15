b0cc1b9b623d31a8e61ce2c7a5f52c3b4b28666d
ljmanini <58728166+lmanini@users.noreply.github.com>
2024-05-15 12:32:40 +0200

    Update DN404_exp.sol
    Fixes project's compilation issue as the contract can't find 'interface.sol'

--- src/test/2024-02/DN404_exp.sol
+++ src/test/2024-02/DN404_exp.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.15;
 
 import "forge-std/Test.sol";
-import "./interface.sol";
+import "../interface.sol";
 
 // @KeyInfo - Total Lost : 200K
 // Attacker : https://etherscan.io/address/0xd215ffaf0f85fb6f93f11e49bd6175ad58af0dfd
