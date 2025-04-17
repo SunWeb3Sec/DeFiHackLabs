928d2d6de11052a81b7b50320ecd4172f9309b12
rotcivegaf <victorfage@gmail.com>
2025-04-17 15:01:00 -0300

    Add IPancakeV3Factory interface

--- src/test/interface.sol
+++ src/test/interface.sol
@@ -5942,6 +5942,128 @@ interface IPancakeV3Pool is
     IPancakeV3PoolOwnerActions
 {}
 
+/// @title The interface for the PancakeSwap V3 Factory
+/// @notice The PancakeSwap V3 Factory facilitates creation of PancakeSwap V3 pools and control over the protocol fees
+interface IPancakeV3Factory {
+    struct TickSpacingExtraInfo {
+        bool whitelistRequested;
+        bool enabled;
+    }
+
+    /// @notice Emitted when the owner of the factory is changed
+    /// @param oldOwner The owner before the owner was changed
+    /// @param newOwner The owner after the owner was changed
+    event OwnerChanged(address indexed oldOwner, address indexed newOwner);
+
+    /// @notice Emitted when a pool is created
+    /// @param token0 The first token of the pool by address sort order
+    /// @param token1 The second token of the pool by address sort order
+    /// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
+    /// @param tickSpacing The minimum number of ticks between initialized ticks
+    /// @param pool The address of the created pool
+    event PoolCreated(
+        address indexed token0,
+        address indexed token1,
+        uint24 indexed fee,
+        int24 tickSpacing,
+        address pool
+    );
+
+    /// @notice Emitted when a new fee amount is enabled for pool creation via the factory
+    /// @param fee The enabled fee, denominated in hundredths of a bip
+    /// @param tickSpacing The minimum number of ticks between initialized ticks for pools created with the given fee
+    event FeeAmountEnabled(uint24 indexed fee, int24 indexed tickSpacing);
+
+    event FeeAmountExtraInfoUpdated(uint24 indexed fee, bool whitelistRequested, bool enabled);
+
+    event WhiteListAdded(address indexed user, bool verified);
+
+    /// @notice Emitted when LM pool deployer is set
+    event SetLmPoolDeployer(address indexed lmPoolDeployer);
+
+    /// @notice Returns the current owner of the factory
+    /// @dev Can be changed by the current owner via setOwner
+    /// @return The address of the factory owner
+    function owner() external view returns (address);
+
+    /// @notice Returns the tick spacing for a given fee amount, if enabled, or 0 if not enabled
+    /// @dev A fee amount can never be removed, so this value should be hard coded or cached in the calling context
+    /// @param fee The enabled fee, denominated in hundredths of a bip. Returns 0 in case of unenabled fee
+    /// @return The tick spacing
+    function feeAmountTickSpacing(uint24 fee) external view returns (int24);
+
+    /// @notice Returns the tick spacing extra info
+    /// @dev A fee amount can never be removed, so this value should be hard coded or cached in the calling context
+    /// @param fee The enabled fee, denominated in hundredths of a bip. Returns 0 in case of unenabled fee
+    /// @return whitelistRequested The flag whether should be created by white list users only
+    function feeAmountTickSpacingExtraInfo(uint24 fee) external view returns (bool whitelistRequested, bool enabled);
+
+    /// @notice Returns the pool address for a given pair of tokens and a fee, or address 0 if it does not exist
+    /// @dev tokenA and tokenB may be passed in either token0/token1 or token1/token0 order
+    /// @param tokenA The contract address of either token0 or token1
+    /// @param tokenB The contract address of the other token
+    /// @param fee The fee collected upon every swap in the pool, denominated in hundredths of a bip
+    /// @return pool The pool address
+    function getPool(
+        address tokenA,
+        address tokenB,
+        uint24 fee
+    ) external view returns (address pool);
+
+    /// @notice Creates a pool for the given two tokens and fee
+    /// @param tokenA One of the two tokens in the desired pool
+    /// @param tokenB The other of the two tokens in the desired pool
+    /// @param fee The desired fee for the pool
+    /// @dev tokenA and tokenB may be passed in either order: token0/token1 or token1/token0. tickSpacing is retrieved
+    /// from the fee. The call will revert if the pool already exists, the fee is invalid, or the token arguments
+    /// are invalid.
+    /// @return pool The address of the newly created pool
+    function createPool(
+        address tokenA,
+        address tokenB,
+        uint24 fee
+    ) external returns (address pool);
+
+    /// @notice Updates the owner of the factory
+    /// @dev Must be called by the current owner
+    /// @param _owner The new owner of the factory
+    function setOwner(address _owner) external;
+
+    /// @notice Enables a fee amount with the given tickSpacing
+    /// @dev Fee amounts may never be removed once enabled
+    /// @param fee The fee amount to enable, denominated in hundredths of a bip (i.e. 1e-6)
+    /// @param tickSpacing The spacing between ticks to be enforced for all pools created with the given fee amount
+    function enableFeeAmount(uint24 fee, int24 tickSpacing) external;
+
+    /// @notice Set an address into white list
+    /// @dev Address can be updated by owner with boolean value false
+    /// @param user The user address that add into white list
+    function setWhiteListAddress(address user, bool verified) external;
+
+    /// @notice Set a fee amount extra info
+    /// @dev Fee amounts can be updated by owner with extra info
+    /// @param whitelistRequested The flag whether should be created by owner only
+    /// @param enabled The flag is the fee is enabled or not
+    function setFeeAmountExtraInfo(
+        uint24 fee,
+        bool whitelistRequested,
+        bool enabled
+    ) external;
+
+    function setLmPoolDeployer(address _lmPoolDeployer) external;
+
+    function setFeeProtocol(address pool, uint32 feeProtocol0, uint32 feeProtocol1) external;
+
+    function collectProtocol(
+        address pool,
+        address recipient,
+        uint128 amount0Requested,
+        uint128 amount1Requested
+    ) external returns (uint128 amount0, uint128 amount1);
+
+    function setLmPool(address pool, address lmPool) external;
+}
+
 interface ILoanTokenLogicWeth {
     function mintWithEther(
         address receiver
